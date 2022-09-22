e6a83119a73e0907ab7ad5286f0163927628fdcd
Varsha_bala <balava@mail.uc.edu>
2022-09-22 15:13:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numOne = 22;
+        int numTwo = 12;
+
+        if(numOne < numTwo)
+        {
+            System.out.println(numOne+" is lesser than "+numTwo);
+        }
+        else if(numOne == numTwo)
+        {
+            System.out.println(numOne+" is equal to "+numTwo);
+        }
+        else if(numOne > numTwo)
+        {
+            System.out.println(numOne+" is greater than "+numTwo);
+        }
+    }
+}
